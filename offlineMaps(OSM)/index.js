b458877122ab430412366cd89a69237c2dda0ba1
Yevhen Guba <yevhen.guba@gmail.com>
2015-08-27 00:07:37 +0300

    online maps examples were added. offline custom styled map example was added.

--- offlineMaps(OSM)/index.js
+++ offlineMaps(OSM)/index.js
@@ -1,10 +1,37 @@
 (function() {
-    var mapsSettings = {
-            city: {
+    var MAP_BOX = {
+            MAP_ID: 'sheenigami.n9a5aip6',
+            API_TOKEN: 'pk.eyJ1Ijoic2hlZW5pZ2FtaSIsImEiOiIzMjMwYTQ4MThiMGRhNjhkZjFlZjUzYzNhNjU5YTdjMCJ9.pZkij_cFCIleZdUg9ChwVg#11'
+        },
+        mapsCreationSettings = [
+            {
                 // for usual rendering of tileLayer source should be http://{s}.tile.osm.org/{z}/{x}/{y}.png
                 // for offline - images/mapTiles/city/{z}/{x}/{y}.png.tile (where .png.tile - type of tile
                 // can be .png, .jpeg, .png.tile, .jpeg.tile, ect.).
                 // tiles were created by Atlas Creator.
+                selector: 'city-online-standard',
+                TILE_SRC: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+                popupMessage: 'This is the center of Kharkiv!',
+                currentZoom: 15,
+                latLng: [49.99, 36.23],
+                options: {
+                    minZoom: 10,
+                    maxZoom: 18
+                }
+            },
+            {
+                selector: 'city-online-custom',
+                TILE_SRC: 'https://a.tiles.mapbox.com/v4/' + MAP_BOX.MAP_ID + '/{z}/{x}/{y}.png?access_token=' + MAP_BOX.API_TOKEN,
+                popupMessage: 'This is the center of Kharkiv!',
+                currentZoom: 14,
+                latLng: [49.99, 36.23],
+                options: {
+                    minZoom: 8,
+                    maxZoom: 18
+                }
+            },
+            {
+                selector: 'city-offline-standard',
                 TILE_SRC: 'images/mapTiles/city/{z}/{x}/{y}.png.tile',
                 popupMessage: 'This is the center of Kharkiv!',
                 currentZoom: 13,
@@ -14,29 +41,40 @@
                     maxZoom: 16
                 }
             },
-            street: {
-                TILE_SRC: 'images/mapTiles/street/{z}/{x}/{y}.jpg.tile',
+            {
+                selector: 'city-offline-custom',
+                TILE_SRC: 'images/mapTiles/cityCustom/{z}/{x}/{y}.png.tile',
                 popupMessage: 'This is the Space Hall location!',
-                currentZoom: 16,
-                latLng: [50.015, 36.220518],
+                currentZoom: 13,
+                latLng: [49.99, 36.23],
                 options: {
-                    minZoom: 14,
-                    maxZoom: 18
+                    minZoom: 9,
+                    maxZoom: 16
                 }
             }
-        },
-        city = mapsSettings.city,
-        street = mapsSettings.street,
-        cityMap = L.map('city').setView(city.latLng, city.currentZoom),
-        streetMap = L.map('street').setView(street.latLng, street.currentZoom);
+        ],
+        maps = [],
+        markers = [];
 
-    L.tileLayer(city.TILE_SRC, city.options).addTo(cityMap);
-    L.marker(city.latLng).addTo(cityMap)
-        .bindPopup(city.popupMessage)
-        .openPopup();
+    function initAllMaps() {
+        mapsCreationSettings.forEach(initMap);
+    }
 
-    L.tileLayer(street.TILE_SRC, street.options).addTo(streetMap);
-    L.marker(street.latLng).addTo(streetMap)
-        .bindPopup(street.popupMessage)
-        .openPopup();
+    function initMap(options) {
+        var map = L.map(options.selector).setView(options.latLng, options.currentZoom);
+
+        L.tileLayer(options.TILE_SRC, options.options).addTo(map);
+        initMarker(options, map);
+        maps.push(map);
+    }
+
+    function initMarker(options, map) {
+        var marker =  L.marker(options.latLng).addTo(map)
+            .bindPopup(options.popupMessage)
+            .openPopup();
+
+        markers.push(marker);
+    }
+
+    initAllMaps();
 }());
